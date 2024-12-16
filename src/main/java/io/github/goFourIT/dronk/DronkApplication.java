eb533c09d6cce4d6fb0abdcc633189601dd9da0f
Cezar <biculescu.cezar@gmail.com>
2024-12-16 13:54:19 +0200

    initialize project

---
+++ src/main/java/io/github/goFourIT/dronk/DronkApplication.java
@@ -0,0 +1,13 @@
+package io.github.goFourIT.dronk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronkApplication.class, args);
+	}
+
+}
